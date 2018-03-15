7bcc21fcfce9a02dfd455bbef290bd2664c651c1
byoungjin <kbj2043@gmail.com>
2018-03-16 01:13:16 +0900

    프레그먼트 전환 메인, 맵 프레그먼트 추가

--- app/src/main/java/com/dac/gapp/andac/MainActivity.kt
+++ app/src/main/java/com/dac/gapp/andac/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -19,15 +20,15 @@ class MainActivity : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+
+                changeFragment(MainFragment())
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+                changeFragment(MapFragment())
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -39,5 +40,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        if (savedInstanceState != null) {
+            return
+        }
+
+        // Create a new Fragment to be placed in the activity layout
+        val firstFragment = MainFragment()
+
+        // In case this activity was started with special instructions from an
+        // Intent, pass the Intent's extras to the fragment as arguments
+        firstFragment.arguments = intent.extras
+
+        // Add the fragment to the 'fragment_container' FrameLayout
+        supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, firstFragment).commit()
+    }
+
+    private fun changeFragment(newFragment: Fragment){
+        // Create fragment and give it an argument specifying the article it should show
+        val args = Bundle()
+        newFragment.arguments = args
+
+        val transaction = supportFragmentManager.beginTransaction()
+
+        // Replace whatever is in the fragment_container view with this fragment,
+        // and add the transaction to the back stack so the user can navigate back
+        transaction.replace(R.id.fragment_container, newFragment)
+        transaction.addToBackStack(null)
+
+        // Commit the transaction
+        transaction.commit()
     }
 }
