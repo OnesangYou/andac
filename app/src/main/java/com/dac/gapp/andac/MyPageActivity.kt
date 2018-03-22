c424366b791b13291980e5e5ad16216ffeef741e
byoungjin <kbj2043@gmail.com>
2018-03-23 00:53:57 +0900

    페이스북 로그인 콜백구현 (토큰으로 파베 가입시 콜백이 안옴. 디버그필요)

--- app/src/main/java/com/dac/gapp/andac/MyPageActivity.kt
+++ app/src/main/java/com/dac/gapp/andac/MyPageActivity.kt
@@ -1,7 +1,9 @@
 package com.dac.gapp.andac
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.dac.gapp.andac.user.LoginActivity
 import kotlinx.android.synthetic.main.activity_my_page.*
 
 class MyPageActivity : AppCompatActivity() {
@@ -11,5 +13,9 @@ class MyPageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_my_page)
 
         back.setOnClickListener({ finish() })
+        goToLogin.setOnClickListener { 
+            val nextIntent = Intent(this, LoginActivity::class.java)
+            startActivity(nextIntent)
+        }
     }
 }
---
+++ app/src/main/java/com/dac/gapp/andac/user/LoginActivity.kt
@@ -0,0 +1,94 @@
+package com.dac.gapp.andac.user
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.dac.gapp.andac.R
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
+
+
+open class LoginActivity : Activity() {
+
+    val TAG = "KBJ"
+    private var mCallbackManager : CallbackManager? = null
+    private var mAuth: FirebaseAuth? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance()
+
+        // Initialize Facebook Login button
+        mCallbackManager = CallbackManager.Factory.create()
+        facebookLoginBtn.setReadPermissions("email", "public_profile")
+        facebookLoginBtn.registerCallback(mCallbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult)
+                handleFacebookAccessToken(loginResult.accessToken)
+            }
+
+            override fun onCancel() {
+                Log.d(TAG, "facebook:onCancel")
+                // ...
+            }
+
+            override fun onError(error: FacebookException) {
+                Log.d(TAG, "facebook:onError", error)
+                // ...
+            }
+        })
+
+    }
+
+    private fun handleFacebookAccessToken(token: AccessToken) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token)
+
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        mAuth!!.signInWithCredential(credential)
+                .addOnCompleteListener(this) { task ->
+                    Log.d(TAG, "complete!!!")
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithCredential:success")
+                        val user = mAuth!!.currentUser
+                        updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithCredential:failure", task.exception)
+                        Toast.makeText(this@LoginActivity, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+                        updateUI(null)
+                    }
+                }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Pass the activity result back to the Facebook SDK
+        mCallbackManager!!.onActivityResult(requestCode, resultCode, data)
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = mAuth!!.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+        Toast.makeText(this@LoginActivity, currentUser.toString(), Toast.LENGTH_SHORT).show()
+    }
+
+}
