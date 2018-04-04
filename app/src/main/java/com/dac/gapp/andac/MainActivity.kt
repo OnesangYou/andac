4c0000dc92b55f1bd7df7b6d141a635221fefb5f
Onesang You <sc545@mutecsoft.com>
2018-04-04 22:05:34 +0900

    BottomNavigationView shifting mode disable

--- app/src/main/java/com/dac/gapp/andac/MainActivity.kt
+++ app/src/main/java/com/dac/gapp/andac/MainActivity.kt
@@ -1,21 +1,26 @@
 package com.dac.gapp.andac
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.internal.BottomNavigationItemView
+import android.support.design.internal.BottomNavigationMenuView
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.util.Log
 import android.view.View
 import com.dac.gapp.andac.fragment.*
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     // static method
     companion object {
-        fun createIntent(context : Context): Intent {
+        fun createIntent(context: Context): Intent {
             return Intent(context, MainActivity::class.java)
         }
     }
@@ -24,18 +29,23 @@ class MainActivity : AppCompatActivity() {
         when (item.itemId) {
             R.id.navigation_main -> {
                 changeFragment(MainFragment())
+                return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_search_hospital -> {
                 changeFragment(SearchHospitalFragment())
+                return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_chat -> {
                 changeFragment(ChatRoomFragment())
+                return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_board -> {
                 changeFragment(BoardFragment())
+                return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_event -> {
                 changeFragment(EventListFragment())
+                return@OnNavigationItemSelectedListener true
             }
         }
         false
@@ -55,6 +65,7 @@ class MainActivity : AppCompatActivity() {
         actionBar.setDisplayHomeAsUpEnabled(false) // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        BottomNavigationViewHelper.removeShiftMode(navigation)
 
         if (savedInstanceState != null) {
             return
@@ -78,7 +89,7 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    private fun changeFragment(newFragment: Fragment){
+    private fun changeFragment(newFragment: Fragment) {
         // Create fragment and give it an argument specifying the article it should show
         val args = Bundle()
         newFragment.arguments = args
@@ -92,5 +103,30 @@ class MainActivity : AppCompatActivity() {
 
         // Commit the transaction
         transaction.commit()
+    }
+
+    internal object BottomNavigationViewHelper {
+
+        @SuppressLint("RestrictedApi")
+        fun removeShiftMode(view: BottomNavigationView) {
+            val menuView = view.getChildAt(0) as BottomNavigationMenuView
+            try {
+                val shiftingMode = menuView.javaClass.getDeclaredField("mShiftingMode")
+                shiftingMode.isAccessible = true
+                shiftingMode.setBoolean(menuView, false)
+                shiftingMode.isAccessible = false
+                for (i in 0 until menuView.childCount) {
+                    val item = menuView.getChildAt(i) as BottomNavigationItemView
+                    item.setShiftingMode(false)
+                    // set once again checked value, so view will be updated
+                    item.setChecked(item.itemData.isChecked)
+                }
+            } catch (e: NoSuchFieldException) {
+                Log.e("ERROR NO SUCH FIELD", "Unable to get shift mode field")
+            } catch (e: IllegalAccessException) {
+                Log.e("ERROR ILLEGAL ALG", "Unable to change value of shift mode")
+            }
+
+        }
     }
 }
