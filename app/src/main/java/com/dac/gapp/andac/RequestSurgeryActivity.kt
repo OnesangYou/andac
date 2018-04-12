81590fb391110505ecdb0c22bcf96583678ffe61
GODueol <weogjw01@naver.com>
2018-04-13 00:47:55 +0900

    add] 상담신청 테스트

--- app/src/main/java/com/dac/gapp/andac/RequestSurgeryActivity.kt
+++ app/src/main/java/com/dac/gapp/andac/RequestSurgeryActivity.kt
@@ -1,12 +1,26 @@
 package com.dac.gapp.andac
 
 import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
+import com.dac.gapp.andac.adapter.SurgeryTypeSelectPagerAdapter
+import kotlinx.android.synthetic.main.activity_request_surgery.*
 
 class RequestSurgeryActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_request_surgery)
+
+        val mViewPager: ViewPager = tabViwePager
+        mViewPager.adapter = SurgeryTypeSelectPagerAdapter(supportFragmentManager)
+
+        val mTabLayout: TabLayout = tablayout
+        mTabLayout.setupWithViewPager(mViewPager)
+
+        mTabLayout.getTabAt(0)!!.setText("오픈형")
+        mTabLayout.getTabAt(1)!!.setText("지정형")
     }
 }
---
+++ app/src/main/java/com/dac/gapp/andac/fragment/DesignationConsultingFragment.kt
@@ -0,0 +1,24 @@
+package com.dac.gapp.andac.fragment
+
+import android.support.v4.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dac.gapp.andac.R
+import kotlinx.android.synthetic.main.fragment_designation_consulting.*
+
+/**
+ * Created by Administrator on 2018-04-13.
+ */
+class DesignationConsultingFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.fragment_designation_consulting, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        textView3.setText("22222222222")
+    }
+}
