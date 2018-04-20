4bfaa550099353719e0c8593d65e10cd1048251e
Onesang You <sc545@mutecsoft.com>
2018-04-20 18:46:05 +0900

    library 정리

---
+++ app/src/main/java/com/dac/gapp/andac/model/HospitalInfo.kt
@@ -0,0 +1,3 @@
+package com.dac.gapp.andac.model
+
+data class HospitalInfo(var address1: Int, var address2: Int, var name: String, var number: String, var openDate: String, var phone: String, var status: String, var type: String)
