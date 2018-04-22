6ced0740db935d5417da9849d12785f9204b730c
ByoungJin <kbj2043@gmail.com>
2018-04-23 00:52:34 +0900

    [Fix] 권한이 모두 획득되지 않았는데 다음 액티비티 넘어가는 버그 픽스
    하지만 스플래시에서 토스트가 띄어지지 않는 문제..남음

--- app/src/user/java/com/dac/gapp/andac/SplashActivity.kt
+++ app/src/user/java/com/dac/gapp/andac/SplashActivity.kt
@@ -5,7 +5,10 @@ import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import java.util.*
+
+
 
 class SplashActivity : AppCompatActivity() {
 
@@ -13,35 +16,41 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        var isDonePermission = true
+        for(it in permissions){
+            // Here, thisActivity is the current activity
+            if (ContextCompat.checkSelfPermission(this,
+                            it)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                                it)) {
+                    // Show an expanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+                    // 토스트가 안띄어짐 ㅠ
+                    Toast.makeText(this@SplashActivity, "권한이 거절되어 앱이 종료됩니다\n권한 승낙후 앱을 다시 실행해주세요", Toast.LENGTH_SHORT).show()
+                    finish()
+                    return
 
-        permissions.stream()
-                .forEach({
-                    // Here, thisActivity is the current activity
-                    if (ContextCompat.checkSelfPermission(this,
-                                    it)
-                            != PackageManager.PERMISSION_GRANTED) {
-                        // Should we show an explanation?
-                        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                                        it)) {
-                            // Show an expanation to the user *asynchronously* -- don't block
-                            // this thread waiting for the user's response! After the user
-                            // sees the explanation, try again to request the permission.
+                } else {
+                    // No explanation needed, we can request the permission.
+                    ActivityCompat.requestPermissions(this,
+                            permissions.toTypedArray(),
+                            999)
 
-                        } else {
-                            // No explanation needed, we can request the permission.
-                            ActivityCompat.requestPermissions(this,
-                                    permissions.toTypedArray(),
-                                    999)
+                    // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                    // app-defined int constant. The callback method gets the
+                    // result of the request.
+                    isDonePermission = false
+                }
+            }
+        }
 
-                            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                            // app-defined int constant. The callback method gets the
-                            // result of the request.
-                        }
-                    } else {
-                        startActivity(MainActivity.createIntent(this))
-                        finish()
-                    }
-                })
+        if(isDonePermission){
+            startActivity(MainActivity.createIntent(this))
+            finish()
+        }
 
     }
 
@@ -61,6 +70,9 @@ class SplashActivity : AppCompatActivity() {
 
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
+                    // 토스트가 안띄어짐 ㅠ
+                    Toast.makeText(this, "권한이 거절되어 앱이 종료됩니다\n권한 승낙후 앱을 다시 실행해주세요", Toast.LENGTH_SHORT).show()
+                    finish()
                 }
                 return
             }
