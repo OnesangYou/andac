6989c9a728a7d502b9fecd9a1292301d1b7d4671
ByoungJin <kbj2043@gmail.com>
2018-06-08 12:13:17 +0900

    [Add] 게시판 글쓰기, 병원 검색 추가

--- app/src/main/java/com/dac/gapp/andac/BoardWriteActivity.kt
+++ app/src/main/java/com/dac/gapp/andac/BoardWriteActivity.kt
@@ -1,12 +1,38 @@
 package com.dac.gapp.andac
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import com.dac.gapp.andac.model.firebase.HospitalInfo
+import kotlinx.android.synthetic.main.activity_board_write.*
 
-class BoardWriteActivity : AppCompatActivity() {
+
+class BoardWriteActivity : com.dac.gapp.andac.base.BaseActivity() {
+
+    private val HOSPITAL_OBJECT_REQUEST = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_board_write)
+
+        hospital_search.setOnClickListener {
+            // 병원 검색
+            Intent(this@BoardWriteActivity, HospitalTextSearchActivity::class.java).let {
+                startActivityForResult(it, HOSPITAL_OBJECT_REQUEST)
+            }
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == RESULT_OK && requestCode == HOSPITAL_OBJECT_REQUEST) {
+            data?.let{
+                (it.getSerializableExtra("hospitalInfo") as HospitalInfo).let { hospitalInfo ->
+                    hospital_search.setText(hospitalInfo.name)
+                }
+            }
+        }
+
     }
 }
--- app/src/main/java/com/dac/gapp/andac/HospitalTextSearchActivity.kt
+++ app/src/main/java/com/dac/gapp/andac/HospitalTextSearchActivity.kt
@@ -1,11 +1,16 @@
 package com.dac.gapp.andac
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+import android.view.View
 import com.algolia.instantsearch.helpers.InstantSearch
 import com.algolia.instantsearch.helpers.Searcher
 import com.dac.gapp.andac.base.BaseActivity
-import com.dac.gapp.andac.R
 import com.dac.gapp.andac.model.Algolia
+import com.dac.gapp.andac.model.firebase.HospitalInfo
+import kotlinx.android.synthetic.main.activity_hospital_text_search.*
 
 
 class HospitalTextSearchActivity : BaseActivity() {
@@ -19,6 +24,30 @@ class HospitalTextSearchActivity : BaseActivity() {
         searcher = Searcher.create(Algolia.APP_ID.value, Algolia.SEARCH_API_KEY.value, Algolia.INDEX_NAME_HOSPITAL.value)
         InstantSearch(this, searcher) // Initialize InstantSearch in this activity with searcher
         searcher.search(intent) // Show results for empty query (on app launch) / voice query (from intent)
+
+        // hospitalInfo 객체를 만들어서 호출한 곳에 보냄
+        hits.setOnItemClickListener{ _: RecyclerView, i: Int, _: View ->
+
+            val hit = hits.get(i)
+
+            HospitalInfo().apply{
+                _geoloc.lat = hit.getJSONObject("_geoloc").getDouble("lat")
+                _geoloc.lng = hit.getJSONObject("_geoloc").getDouble("lng")
+                address1 = hit.getString("address1")
+                address2 = hit.getString("address2")
+                name = hit.getString("name")
+                openDate = hit.getString("openDate")
+                phone = hit.getString("phone")
+                status = hit.getString("status")
+                type = hit.getString("type")
+            }.let { hospitalInfo ->
+                Intent().let{
+                    it.putExtra("hospitalInfo", hospitalInfo)
+                    setResult(Activity.RESULT_OK, it)
+                    finish()
+                }
+            }
+        }
     }
 
     override fun onDestroy() {
