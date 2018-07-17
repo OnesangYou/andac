3245e7020c7698a56c10c185270ff194ef3fa25d
gimbyeongjin <kbj2043@gmail.com>
2018-07-17 23:48:21 +0900

    [Add] 마이페이지 즐겨찾기 추가

--- app/src/user/java/com/dac/gapp/andac/MyPageActivity.kt
+++ app/src/user/java/com/dac/gapp/andac/MyPageActivity.kt
@@ -1,8 +1,11 @@
 package com.dac.gapp.andac
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import com.bumptech.glide.Glide
 import com.dac.gapp.andac.base.BaseMyPageActivity
+import com.dac.gapp.andac.fragment.AccountSettingFragment
+import com.dac.gapp.andac.fragment.FavoritesFragment
 import kotlinx.android.synthetic.user.activity_my_page.*
 
 open class MyPageActivity : BaseMyPageActivity() {
@@ -18,10 +21,39 @@ open class MyPageActivity : BaseMyPageActivity() {
             finish()
         }
 
+        myPageRadioGroup.setOnCheckedChangeListener { _, id ->
+            when(id){
+                R.id.accountSettingBtn -> changeFragment(AccountSettingFragment())
+                R.id.favoritBtn -> changeFragment(FavoritesFragment())
+
+                else -> {
+//                    accountSetting.visibility = View.INVISIBLE
+//                    recyclerView.visibility = View.VISIBLE
+                }
+            }
+        }
+
         // Set Profile
         getUserInfo()?.addOnSuccessListener { userInfo ->
             if(userInfo.profilePicUrl.isNotEmpty()) Glide.with(this@MyPageActivity).load(userInfo.profilePicUrl).into(profilePic)
             nameText.text = userInfo.nickName
         }
     }
+
+    private fun changeFragment(newFragment: Fragment) {
+        // Create fragment and give it an argument specifying the article it should show
+        val args = Bundle()
+        newFragment.arguments = args
+
+        val transaction = supportFragmentManager.beginTransaction()
+
+        // Replace whatever is in the fragment_container view with this fragment,
+        // and add the transaction to the back stack so the user can navigate back
+        transaction.replace(R.id.fragment_container, newFragment)
+        transaction.addToBackStack(null)
+
+        // Commit the transaction
+        transaction.commit()
+    }
+
 }
---
+++ app/src/user/java/com/dac/gapp/andac/fragment/AccountSettingFragment.kt
@@ -0,0 +1,22 @@
+package com.dac.gapp.andac.fragment
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.dac.gapp.andac.R
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class AccountSettingFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account_setting, container, false)
+    }
+
+}// Required empty public constructor
