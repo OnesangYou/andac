153140d8b327c79afe85ccd6a6a81f9b6c520fac
gimbyeongjin <kbj2043@gmail.com>
2018-09-19 19:45:17 +0900

    [Fix] BaseFragment BaseActivity 동기화

--- app/src/main/java/com/dac/gapp/andac/base/BaseFragment.kt
+++ app/src/main/java/com/dac/gapp/andac/base/BaseFragment.kt
@@ -1,42 +1,28 @@
 package com.dac.gapp.andac.base
 
-import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.databinding.ViewDataBinding
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.dac.gapp.andac.LoginActivity
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 
 open class BaseFragment : Fragment() {
-    val GOTO_MYPAGE = "goToMyPage"
 
     override fun getContext(): BaseActivity? {
         return super.getContext() as BaseActivity?
     }
 
-    fun getAuth(): FirebaseAuth? = FirebaseAuth.getInstance()
+    val GOTO_MYPAGE = context?.GOTO_MYPAGE
 
-    fun getCurrentUser(): FirebaseUser? = getAuth()?.currentUser
-            ?.also {
-                it.phoneNumber?.isEmpty()?.let { isEmpty ->
-                    if (isEmpty) goToLogin()
-                }
-            } // 폰등록 안되 있는 경우는 login 이동
+    fun getAuth() = context?.getAuth()
 
-    fun getUid(): String? {
-        return getCurrentUser()?.uid
-    }
+    fun getCurrentUser() = context?.getCurrentUser()
 
-    fun goToLogin(gotoMyPage: Boolean = false) {
-        Intent(context, LoginActivity::class.java).let {
-            if (gotoMyPage) it.putExtra(GOTO_MYPAGE, true)
-            startActivity(it)
-        }
-    }
+    fun getUid() = context?.getUid()
+
+
+    fun goToLogin(gotoMyPage: Boolean = false) = context?.goToLogin(gotoMyPage)
 
     private var viewDataBinding: ViewDataBinding? = null
 
