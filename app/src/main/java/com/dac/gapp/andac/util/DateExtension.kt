8995dfbcc8db311bd61dadb2f3ac6f2e4c1eb8b7
Byoungjin <kbj2043@gmail.com>
2018-09-24 04:09:55 +0900

    [Add] [유저] 회원가입 : 생년월일, 성별 데이터 추가

--- app/src/main/java/com/dac/gapp/andac/util/DateExtension.kt
+++ app/src/main/java/com/dac/gapp/andac/util/DateExtension.kt
@@ -5,3 +5,4 @@ import java.util.*
 
 fun Date.getFullFormat(): String? = SimpleDateFormat("yyyy/MM/dd HH:mm", Locale.KOREA).format(this)
 fun Date.getDateFormat(char: Char): String? = SimpleDateFormat("yy${char}MM${char}dd", Locale.KOREA).format(this)
+fun Date.getDateFormat(pattern: String): String? = SimpleDateFormat(pattern, Locale.KOREA).format(this)
