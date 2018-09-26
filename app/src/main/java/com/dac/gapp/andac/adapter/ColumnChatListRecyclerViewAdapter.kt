2875716182d60774243bd96fb3ad6caeac5269dd
godueol <weogjw01@naver.com>
2018-09-27 00:15:51 +0900

    [Add] 채팅기능 추가중 1.0

--- app/src/main/java/com/dac/gapp/andac/adapter/ColumnChatListRecyclerViewAdapter.kt
+++ app/src/main/java/com/dac/gapp/andac/adapter/ColumnChatListRecyclerViewAdapter.kt
@@ -5,29 +5,74 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.dac.gapp.andac.R
-import com.dac.gapp.andac.model.ChatMessage
-import com.dac.gapp.andac.viewholder.ColumnChatListViewHolder
+import com.dac.gapp.andac.model.firebase.ChatItem
+import com.dac.gapp.andac.viewholder.*
+import timber.log.Timber
 
 /**
  * Created by godueol on 2018. 4. 28..
  */
 
 
-class ColumnChatListRecyclerViewAdapter(var context: Context, var list: List<ChatMessage>) : RecyclerView.Adapter<ColumnChatListViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ColumnChatListViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.column_row_chat, parent, false)
-        return ColumnChatListViewHolder(view)
-    }
-
+class ColumnChatListRecyclerViewAdapter(var list: List<ChatItem>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun getItemCount(): Int {
         return list.size
     }
 
+    override fun onBindViewHolder(mHolder: RecyclerView.ViewHolder, position: Int) {
+        val item: ChatItem = list[position]
+        Timber.d(list.toString())
+//        holder.titleText.text = item.content
 
-    override fun onBindViewHolder(holder: ColumnChatListViewHolder, position: Int) {
-        val item: ChatMessage = list.get(position)
-        holder.titleText.text = item.text
-
+        when (item.type) {
+            0 -> {
+                val holder: ChatMineMessageHolder = mHolder as ChatMineMessageHolder
+                holder.binding.chatItem = item
+            }
+            1 -> {
+                val holder: ChatOtherMessageViewHolder = mHolder as ChatOtherMessageViewHolder
+                holder.binding.chatItem = item
+            }
+            2 -> {
+                val holder: ChatMineImageViewHolder = mHolder as ChatMineImageViewHolder
+                holder.binding.chatItem = item
+            }
+            3 -> {
+                val holder: ChatOtherImageViewHolder = mHolder as ChatOtherImageViewHolder
+                holder.binding.chatItem = item
+            }
+        }
     }
+
+    override fun getItemViewType(position: Int): Int {
+        val message = list[position]
+        return message.type
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        when (viewType) {
+            0 -> {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.chat_mine_message_row, parent, false)
+                return ChatMineMessageHolder(view)
+            }
+            1 -> {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.chat_other_message_row, parent, false)
+                return ChatOtherMessageViewHolder(view)
+            }
+            2 -> {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.chat_mine_image_row, parent, false)
+                return ChatMineImageViewHolder(view)
+            }
+            3 -> {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.chat_other_image_view, parent, false)
+                return ChatOtherImageViewHolder(view)
+            }
+            else -> {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.chat_mine_message_row, parent, false)
+                return ChatMineMessageHolder(view)
+            }
+        }
+    }
+
 
 }
