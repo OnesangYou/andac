15a83d9a2e9f73e4d38a52bcc4eae42a5757cb4d
Byoungjin <kbj2043@gmail.com>
2018-10-07 01:01:56 +0900

    [Add] 플스 버전 업데이트 받아야하면 받게하기

--- app/src/user/java/com/dac/gapp/andac/SplashActivity.kt
+++ app/src/user/java/com/dac/gapp/andac/SplashActivity.kt
@@ -2,36 +2,41 @@ package com.dac.gapp.andac
 
 import android.app.Activity
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import com.dac.gapp.andac.base.BaseActivity
 import com.dac.gapp.andac.util.MyToast
+import com.dac.gapp.andac.util.RemoteConfig
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.TedPermission
 import java.util.*
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        checkGooglePlayServices()
+        RemoteConfig.init().addOnSuccessListener {
+            TedPermission.with(this)
+                    .setPermissionListener(object : PermissionListener {
+                        override fun onPermissionGranted() {
+                            checkMarketVersion(){
+                                startActivity(MainActivity.createIntent(thisActivity()))
+                                finish()
+                            }
+                        }
 
-        TedPermission.with(this)
-                .setPermissionListener(object : PermissionListener {
-                    override fun onPermissionGranted() {
-                        startActivity(MainActivity.createIntent(thisActivity()))
-                        finish()
-                    }
-
-                    override fun onPermissionDenied(deniedPermissions: ArrayList<String>?) {
-                        MyToast.showShort(thisActivity(), "권한이 거절되어 앱이 종료됩니다\n권한 승낙후 앱을 다시 실행해주세요")
-                        finish()
-                    }
-                })
-                .setDeniedMessage("해당 권한이 없을 경우 앱을 사용할 수 없습니다!!")
-                .setPermissions(
-                        android.Manifest.permission.INTERNET,
-                        android.Manifest.permission.ACCESS_FINE_LOCATION,
-                        android.Manifest.permission.CALL_PHONE
-                        )
-                .check()
+                        override fun onPermissionDenied(deniedPermissions: ArrayList<String>?) {
+                            MyToast.showShort(thisActivity(), "권한이 거절되어 앱이 종료됩니다\n권한 승낙후 앱을 다시 실행해주세요")
+                            finish()
+                        }
+                    })
+                    .setDeniedMessage("해당 권한이 없을 경우 앱을 사용할 수 없습니다!!")
+                    .setPermissions(
+                            android.Manifest.permission.INTERNET,
+                            android.Manifest.permission.ACCESS_FINE_LOCATION,
+                            android.Manifest.permission.CALL_PHONE
+                    )
+                    .check()
+        }
     }
 
     fun thisActivity(): Activity {
