51b3413b468f9e27941295fcb0521e2ec9fccf95
gimbyeongjin <kbj2043@gmail.com>
2018-11-03 18:25:11 +0900

    [Modify] 병원 프로필 사진 수정시 삭제 기능 추가

--- app/src/hospital/java/com/dac/gapp/andac/ChangeProfileActivity.kt
+++ app/src/hospital/java/com/dac/gapp/andac/ChangeProfileActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import com.bumptech.glide.Glide
 import com.dac.gapp.andac.base.BaseActivity
 import com.dac.gapp.andac.databinding.ActivityChangeProfileBinding
+import com.dac.gapp.andac.extension.loadImageAny
 import com.dac.gapp.andac.model.firebase.HospitalInfo
 import com.google.firebase.firestore.SetOptions
+import com.google.firebase.storage.FirebaseStorage
+import org.jetbrains.anko.alert
 
 class ChangeProfileActivity : BaseActivity() {
 
@@ -50,6 +53,32 @@ class ChangeProfileActivity : BaseActivity() {
                     ?.addOnSuccessListener { toast("프로필 변경 완료되었습니다"); finish() }
                     ?:hideProgressDialog()
         }
+
+        // 사진 삭제
+        binding.cancleBtn.setOnClickListener { _ ->
+            if(hospitalInfo?.profilePicUrl.isNullOrEmpty()) return@setOnClickListener
+            alert(title = "프로필 사진 지우기", message = "프로필 사진을 삭제하시겠습니까?") {
+
+                positiveButton("YES") { _ ->
+                    showProgressDialog()
+                    FirebaseStorage.getInstance().getReferenceFromUrl(hospitalInfo?.profilePicUrl?:return@positiveButton).delete()
+                            .continueWithTask {
+                                hospitalInfo
+                                        ?.apply { profilePicUrl = "" }
+                                        ?.let { it1 -> getHospital()?.set(it1, SetOptions.merge()) }
+                            }
+                            .addOnSuccessListener {
+                                binding.pictureImage.loadImageAny(android.R.drawable.ic_menu_camera)
+                                toast("프로필 사진을 삭제 완료하였습니다.")
+                            }
+                            .addOnCompleteListener { hideProgressDialog() }
+                            .addOnFailureListener {
+                                it.printStackTrace()
+                            }
+                }
+                negativeButton("NO") {}
+            }.show()
+        }
     }
 
     private fun setOnClickListener(hospitalInfo: HospitalInfo?) {
@@ -59,10 +88,10 @@ class ChangeProfileActivity : BaseActivity() {
                 getUid()?.let { s ->
                     showProgressDialog()
                     getHospitalsStorageRef().child(s).child(profilePicJpgStr).putFile(picUri).continueWith {
-                        toast("uploadPicFile Complete")
+                        toast("프로필 사진 업로드 완료되었습니다")
                         it.result.downloadUrl.toString()
                     }
-                            .addOnSuccessListener { Glide.with(this@ChangeProfileActivity).load(it).into(binding.pictureImage) }
+                            .addOnSuccessListener { binding.pictureImage.loadImageAny(it) }
                             .continueWithTask {
                                 hospitalInfo
                                         ?.apply { profilePicUrl = it.result.toString() }
